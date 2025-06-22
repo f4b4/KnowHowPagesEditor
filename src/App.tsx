6abb801abaca072520701cb93353d52d0c503b28
f4b4 <f4b4@NEO>
2025-06-22 20:41:33 +0200

    install monaco editor

--- src/App.tsx
+++ src/App.tsx
@@ -66,14 +66,6 @@ function App() {
       </aside>
       <main className="main-content">
         <div className="content-header">
-          <div>
-            <a href="https://vite.dev" target="_blank">
-              <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://react.dev" target="_blank">
-              <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-          </div>
           <h1>KnowHow Pages Editor</h1>
         </div>
         
